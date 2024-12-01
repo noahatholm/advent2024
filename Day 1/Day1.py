914e58cbfa3669d1f5716c70bf9d65a140fae57d
noahatholm <noahatholm@gmail.com>
2024-12-01 13:26:58 +0000

    combined into same file

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,15 +1,21 @@
-left = []
-right = []
-dif = []
+
 
 def difference(num1,num2):
     return(abs(int(num1) - int(num2)))
 
+def sim(num, array):
+    return (int(num) * array.count(num))
+
 def sum_array(array):
     sum = 0
-    for i in range(len(dif)):
-        sum += dif[i]
+    for i in range(len(array)):
+        sum += array[i]
     return sum
+
+left = []
+right = []
+dif1 = []
+dif2 = []
 
 f = open("data.txt", "r")
 for i in (f):
@@ -24,10 +30,16 @@ left.sort()
 right.sort()
 
 for i in range(len(left)):
-    dif.append(difference(left[i],right[i]))
+    dif1.append(difference(left[i],right[i]))
 
+for i in range(len(left)):
+    dif2.append(sim(left[i],right))
 
-print(sum_array(dif))
+print(sum_array(dif1))
+
+print(sum_array(dif2))
 
 
 f.close()
+
+
